f74ff77fd81d9772af2f835a7762b819e6d7e1a2
RodrigoPereir4 <digolima167@gmail.com>
2024-08-09 21:37:51 -0300

    Desafio simulando uma conta bancaria pelo terminal

--- DesafioUML/BootCampDIO/src/main/java/Apple/Iphone.java
+++ DesafioUML/BootCampDIO/src/main/java/Apple/Iphone.java
@@ -1,78 +1,73 @@
-
-
 package Apple;
 
 /**
  *
  * @author Rodrigo
  */
-public class Iphone implements Navegador.Internet, Reprodutor.Reprodutor, Telefone.Telefone{
-    
-    
-        
-        
+public class Iphone implements Navegador.Internet, Reprodutor.Reprodutor, Telefone.Telefone {
+
     @Override
     public void pesquisar() {
-        throw new UnsupportedOperationException("Pesquisar alguma coisa"); 
+        throw new UnsupportedOperationException("Pesquisar alguma coisa");
     }
 
     @Override
     public void exibirPagina(String url) {
-        throw new UnsupportedOperationException("Exibindo pagina selecionada"); 
+        throw new UnsupportedOperationException("Exibindo pagina selecionada");
     }
 
     @Override
     public void addNovaABA() {
-        throw new UnsupportedOperationException("Adicionando nova pagina"); 
+        throw new UnsupportedOperationException("Adicionando nova pagina");
     }
 
     @Override
     public void refresh() {
-        throw new UnsupportedOperationException("Atualizando pagina"); 
+        throw new UnsupportedOperationException("Atualizando pagina");
     }
 
     @Override
     public void reproduzir() {
-        throw new UnsupportedOperationException("Reproduzindo a música selecionada"); 
+        throw new UnsupportedOperationException("Reproduzindo a música selecionada");
     }
 
     @Override
     public void selecionarMusica() {
-        throw new UnsupportedOperationException("Selecionando música"); 
+        throw new UnsupportedOperationException("Selecionando música");
     }
 
     @Override
     public void pausar() {
-        throw new UnsupportedOperationException("Música pausada"); 
+        throw new UnsupportedOperationException("Música pausada");
     }
 
     @Override
     public void passarMusica() {
-        throw new UnsupportedOperationException("Passando para a próxima música"); 
+        throw new UnsupportedOperationException("Passando para a próxima música");
     }
 
     @Override
     public void voltarMusica() {
-        throw new UnsupportedOperationException("Voltando música"); 
+        throw new UnsupportedOperationException("Voltando música");
     }
 
     @Override
     public void realizarChamada() {
-        throw new UnsupportedOperationException("Chamando"); 
+        throw new UnsupportedOperationException("Chamando");
     }
 
     @Override
     public void atender() {
-        throw new UnsupportedOperationException("Atender chamada"); 
+        throw new UnsupportedOperationException("Atender chamada");
     }
 
     @Override
     public void iniciarCorreioVoz() {
-        throw new UnsupportedOperationException("Iniciando correio de voz"); 
+        throw new UnsupportedOperationException("Iniciando correio de voz");
     }
 
     @Override
     public void finalizarChamada() {
-        throw new UnsupportedOperationException("Finalizando chamada"); 
+        throw new UnsupportedOperationException("Finalizando chamada");
     }
 }
---
+++ Trilha Java Basico/src/trilha/java/basico/TrilhaJavaBasico.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trilha.java.basico;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author digol
+ */
+public class TrilhaJavaBasico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        
+        int numero;
+        String agencia, nomeCliente;
+        double saldo;
+        
+        System.out.println("Por favor, digite o número da Conta!");
+        numero = ler.nextInt();
+        
+        System.out.println("Agora digite o número da Agência.");
+        agencia = ler.next();
+        
+        System.out.println("Por favor, agora digite seu Nome!");
+        nomeCliente = ler.next();
+        
+        System.out.println("Por fim, digite o seu saldo.");
+        saldo = ler.nextDouble();
+        
+        System.out.printf("\nOlá %s, obrigado por criar uma conta em nosso banco"
+                + " sua agência é %s, conta %d, e seu saldo %2f já está disponivel pra saque\n", 
+                nomeCliente, agencia, numero, saldo);
+    }
+    
+    }
+    
+
